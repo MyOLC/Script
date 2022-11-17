7d50358be8d10845f8e6d6d1d3ec2ebe9ee0a83b
MyOLC <115734188+MyOLC@users.noreply.github.com>
2022-11-17 20:06:04 +0000

    Update OOBEDeploy.ps1

--- OOBEDeploy.ps1
+++ OOBEDeploy.ps1
@@ -1,4 +1,4 @@
-PowerShell -NoL -Com Set-ExecutionPolicy Unrestricted -Force
+Set-ExecutionPolicy Unrestricted -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Install-Module AutopilotOOBE -Force -Verbose
 Install-Module OSD -Force -Verbose
