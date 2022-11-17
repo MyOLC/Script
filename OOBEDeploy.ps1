95e19286cf46f88dd0b1be3763ff18e233c0d01b
MyOLC <115734188+MyOLC@users.noreply.github.com>
2022-11-17 19:56:57 +0000

    Update OOBEDeploy.ps1

--- OOBEDeploy.ps1
+++ OOBEDeploy.ps1
@@ -1,7 +1,7 @@
 PowerShell -NoL -Com Set-ExecutionPolicy Unrestricted -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
-Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+Install-Module AutopilotOOBE -Force -Verbose
+Install-Module OSD -Force -Verbose
 $Params = @{
     AddNetFX3 = $true
     AddRSAT = $false
